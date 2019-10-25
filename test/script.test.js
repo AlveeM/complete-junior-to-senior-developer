72f6be8038cbce4d68a4c3608267da50040d2e92
Alvee Akand <AlveeM@users.noreply.github.com>
2019-10-25 23:20:46 +0600

    added script.test.js

--- test/script.test.js
+++ test/script.test.js
@@ -1,3 +1,26 @@
-it('this is a test', () => {
+const googleSearch = require('./script');
 
+dbMock = [
+    'dog.com',
+    'cheesepuff.com',
+    'disney.com',
+    'dogpictures.com'
+];
+
+it('a silly test', () => {
+    expect('hello').toBe('hello');
+})
+
+describe('googleSearch', () => {
+    it('is searching google', () => {
+        expect(googleSearch('testtest', dbMock)).toEqual([])
+        expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
+
+        expect(googleSearch(undefined, dbMock)).toEqual([]);
+        expect(googleSearch(null, dbMock)).toEqual([]);
+    })
+
+    it('does not return more than 3 matches', () => {
+        expect(googleSearch('.com', dbMock).length).toEqual(3);
+    })
 })
