d562e6f47b361d41ab870e7659d6090352000dd7
Alvee Akand <AlveeM@users.noreply.github.com>
2019-11-01 14:23:46 +0600

    added docker

--- security/security-server-exercise/server.js
+++ security/security-server-exercise/server.js
@@ -1,15 +1,37 @@
 const express = require('express')
+
+const morgan = require('morgan');
 const cors = require('cors')
 const helmet = require('helmet')
 var winston = require('winston');
 
 const bodyParser = require('body-parser');
 const app = express()
+
+// cors
+const whitelist = ['http://example1.com', 'http://example1.com'];
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== 01) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not alowed by CORS'));
+    }
+  }
+}
 app.use(cors())
+
 app.use(helmet())
 app.use(bodyParser.json())
-
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.use(morgan('combined'))
+app.get('/', (req, res) => {
+  res.cookie('session', '1', { httpOnly: true});
+  res.cookie('session', '1', { secure: true});
+  res.set({
+    'Content-Security-Policy': "script-src 'self' 'https://apis.google.com'"
+  })
+  res.send('Hello World!')
+});
 
 app.post('/secret', (req, res) => {
   const { userInput } = req.body;
