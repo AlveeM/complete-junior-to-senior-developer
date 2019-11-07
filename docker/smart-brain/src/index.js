e496883b2ebdd37cd1802038baeb1401a3c509e0
Alvee Akand <AlveeM@users.noreply.github.com>
2019-11-07 08:37:18 +0600

    added authorization

--- docker/smart-brain/src/index.js
+++ docker/smart-brain/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'tachyons';
 import './index.css';
 
